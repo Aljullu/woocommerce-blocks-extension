707f8978790d5b9259b70b1ec4d5ef07b82eab40
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-07-25 20:53:32 +0200

    Add before/after actions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@
 /**
  * External dependencies
  */
-import { addFilter } from '@wordpress/hooks';
+import { addAction, addFilter } from '@wordpress/hooks';
 
 addFilter( 'woocommerce-blocks-review-args', 'woocommerce-blocks-extension', ( data ) => {
-  return { ...data, rating: data.rating * 2 };
+	return { ...data, rating: data.rating * 2 };
+} );
+
+addAction( 'woocommerce-blocks-before-review', 'woocommerce-blocks-extension', ( template, args, context ) => {
+	console.log( `Starting to generate the template for review ${ context.review.id }` );
+} );
+
+addAction( 'woocommerce-blocks-after-review', 'woocommerce-blocks-extension', ( template, args, context ) => {
+	console.log( `Review ${ context.review.id } template ready` );
 } );
 
 addFilter( 'woocommerce-blocks-review-rating-template', 'woocommerce-blocks-extension', () => {
-  return ( { rating } ) => ( <span>&nbsp;{ rating }/10</span> );
+	return ( { rating } ) => ( <span>&nbsp;{ rating }/10</span> );
 } );
